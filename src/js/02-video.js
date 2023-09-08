ed52b7f1a6e5f743b4665401c93b80675d15bbe5
KMIS2008 <lion.irisha@gmail.com>
2023-09-08 19:38:31 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import  throttle  from 'lodash.throttle';
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate ', throttle (event => {
+    localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(event.seconds))
+}, 1000));
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0).catch(function (error) {
+    console.error(error)
+});
+
 
