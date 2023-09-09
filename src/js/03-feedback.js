01a4c07667445391efb11e752573911c84dc6d3e
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2023-09-09 11:02:48 +0300

    task3-1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,5 +2,40 @@ import  throttle  from 'lodash.throttle';
 
 
 const form = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = 'feedback-form-state';
 
-form.addEventListener('input', )
+toolStorage();
+
+function inputText (event) {
+    event.preventDefault();
+
+    const info = {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+
+localStorage.setItem(FEEDBACK_KEY, JSON.stringify(info));
+
+
+}
+
+form.addEventListener('input', throttle(inputText), 500);
+
+function toolStorage(){
+
+    const infoForm = JSON.parse(localStorage.getItem(FEEDBACK_KEY))
+
+    console.log(infoForm)
+    form.elements.email.textContent = infoForm.email.value || "";
+    form.elements.message.textContent = infoForm.message.value || "";
+   
+}
+
+
+function buttonPress(event){
+
+    console.log ()
+    form.reset()
+}
+
+form.addEventListener('submit', buttonPress)
